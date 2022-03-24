9439af0ba995e77092db1245a54aa0478cda1487
Lucas Correia de Melo <01319591@sempreuninassau.com.br>
2022-03-24 20:40:51 -0300

    SUbida de codigo de atividades nassau

---
+++ src/nassau/Exe6.java
@@ -0,0 +1,11 @@
+package nassau;
+
+public class Exe6 {
+	 int a;
+	 
+	 public Exe6 (int a) {
+		 for (int i=1;i<=9;i++) {
+			 System.out.println(i + " x " + a + " = " + i * a);
+		 }
+	 }
+}
---
+++ src/nassau/Exe6_test.java
@@ -0,0 +1,11 @@
+package nassau;
+
+public class Exe6_test {
+
+	public static void main(String[] args) {
+		Exe6 exe6 = new Exe6(10);
+		
+
+	}
+
+}
