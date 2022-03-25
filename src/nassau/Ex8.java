aa01eb7574c45d39505611fdfe4babfce8d3f39b
Lucas Correia de Melo <01319591@sempreuninassau.com.br>
2022-03-24 21:53:35 -0300

    revisar Ex8 java

---
+++ src/nassau/Ex8.java
@@ -0,0 +1,20 @@
+package nassau;
+
+public class Ex8 {
+	
+	public Ex8 () {
+		int[] x = new int[4];
+		x[1] = 30;
+		x[2] = 10;
+		x[3] = 20;
+		x[4] = 40;
+		
+		for(int i = x.length; i>=0 ;i--) {
+			
+		}
+	}
+		
+	public static void main (String [] args) {
+		
+	}
+}
