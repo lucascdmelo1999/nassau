fa6cb589bd7bd7ba9325ea9e3277a099a81d7c9f
Melo <lucas.correia.d.melo@accenture.com>
2022-03-25 15:07:32 -0300

    Ex7 concluido

--- src/AtvJava/Ex7.java
+++ src/AtvJava/Ex7.java
@@ -2,26 +2,25 @@ package AtvJava;
 
 public class Ex7 {
 	
-	int cres;
-	int descre;
-	int i;
+	 int i;
+	 String agradecimento = "Muito obrigado por usar meu sistema";
 	
-	public int crecente(int cres) {
-		i = 0;
-		while (i <= cres ) {
+	public String crecente() {
+		i = -1;
+		while (i <= 8) {
 			i++;
 			System.out.println(i);
 		}
-		return i ;
+		return agradecimento;
 	}
 	
-	public int descrecente(int descre) {
+	public int descrecente() {
 		i = 10;
-		while (i >= descre ) {
+		while (i >= 2 ) {
 			i--;
 			System.out.println(i);
 		}
-		return i ;
+		return (i - 1);
 	}
 
 }
--- src/AtvJava/Exe7_test.java
+++ src/AtvJava/Exe7_test.java
@@ -4,9 +4,11 @@ public class Exe7_test {
 
 	public static void main(String[] args) {
 		Ex7 n = new Ex7();
-		System.out.println(n.crecente(10));
-		
-
+		System.out.println("DESCRECENTE ");
+		System.out.println(n.descrecente());
+		System.out.println("");
+		System.out.println("CRECENTE ");
+		System.out.println(n.crecente());
 	}
 
 }
