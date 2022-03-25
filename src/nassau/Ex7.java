24660279a21a8a8c2792e3764b18216eab52d193
Lucas Correia de Melo <01319591@sempreuninassau.com.br>
2022-03-24 21:18:56 -0300

    Exe 7 incompleto

---
+++ src/nassau/Ex7.java
@@ -0,0 +1,27 @@
+package nassau;
+
+public class Ex7 {
+	
+	int cres;
+	int descre;
+	int i;
+	
+	public int crecente(int cres) {
+		i = 0;
+		while (i <= cres ) {
+			i++;
+			System.out.println(i);
+		}
+		return i ;
+	}
+	
+	public int descrecente(int descre) {
+		i = 10;
+		while (i >= descre ) {
+			i--;
+			System.out.println(i);
+		}
+		return i ;
+	}
+
+}
---
+++ src/nassau/Exe7_test.java
@@ -0,0 +1,12 @@
+package nassau;
+
+public class Exe7_test {
+
+	public static void main(String[] args) {
+		Ex7 n = new Ex7();
+		System.out.println(n.crecente(10));
+		
+
+	}
+
+}
