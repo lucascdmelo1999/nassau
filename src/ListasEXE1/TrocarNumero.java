b15d8dd5cbc1d6787104b3e07f5f19ec1930a845
Melo <lucas.correia.d.melo@accenture.com>
2022-04-08 18:26:22 -0300

    completo 4° da lista

--- src/ListasEXE1/TrocarNumero.java
+++ src/ListasEXE1/TrocarNumero.java
@@ -1,5 +1,17 @@
 package ListasEXE1;
 
-public class TrocarNumero {
+public class Trocarnumero {
+	
+	int num_a;     
+	int num_b;  
+	
+	public Trocarnumero(int num_a, int num_b) {         
+		this.num_a = num_b;         
+		this.num_b = num_a;	   
+	}
+	public String trocarnumero() {
+		
+		return this.num_a + " = A " + this.num_b + " = B";  
+	}
 
 }
---
+++ src/ListasEXE1/test_trocarnumero.java
@@ -0,0 +1,11 @@
+package ListasEXE1;
+
+public class test_trocarnumero {
+
+	public static void main(String[] args) {
+		Trocarnumero tn = new Trocarnumero(4,5);
+		System.out.println(tn.trocarnumero());
+		
+	}
+
+}
